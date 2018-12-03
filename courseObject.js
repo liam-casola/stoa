0ac7cc0b9a975ead0b1624a45569bdcfb70774a9
Liam Casola <liam.casola@gmail.com>
2018-12-03 14:05:22 -0500

    added secondary nav and partial work on auto generated content

---
+++ courseObject.js
@@ -0,0 +1,36 @@
+var courseData = [
+    {
+        label: 'Announcement',
+        icon: 'icons/megaphone.svg',
+        link: 'announcement.html'
+    },
+    {
+        label: 'Calendar',
+        icon: 'icons/calendar.svg',
+        link: 'https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=mcmaster.ca_1070pjmt48obikqbsadt22bduo%40group.calendar.google.com&amp;color=%23865A5A&amp;ctz=America%2FToronto'
+    },
+    {
+        label: 'Overview',
+        icon: 'icons/exam.svg',
+        link: 'course1/outline.pdf'
+    },
+    {
+        label: 'Grades',
+        icon: 'icons/ribbon.svg',
+        link: 'announcement.html'
+    },
+    {
+        label: 'Lectures',
+        icon: 'icons/folder.svg',
+        link: [
+            {
+                label: 'Lecture 4',
+                link: 'course1/lec_1.pdf'
+            },
+            {
+                label: 'Lecture 1',
+                link: 'course1/lec_2.pdf'
+            },
+        ]
+    }
+];
