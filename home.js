08668d073b5fd18fa33122575fca8eeaf2415fae
Liam Casola <liam.casola@gmail.com>
2018-12-05 12:38:02 -0500

    completed styling and missing functionality from homepage, added title text to course header

--- home.js
+++ home.js
@@ -1,3 +1,13 @@
-function hideAnnouncement(element) {
-    
+function hide(element) {
+    uList = element.parentElement.parentElement;
+    element.parentElement.remove();
+    if (uList.innerHTML.trim().length == 0) {
+        listItem = document.createElement('li');
+        listItem.innerHTML = 'no new announcements';
+        uList.appendChild(listItem);
+    }
+}
+
+function storeName() {
+    console.log('name stored');
 }
